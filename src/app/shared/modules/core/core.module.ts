59adba5b119c991a7bc0b704fca807a8abc4da9b
harveybland <harvey.bland@genius.online>
2022-05-02 21:54:02 +0100

    fixed margin, broke sub-menu

--- src/app/shared/modules/core/core.module.ts
+++ src/app/shared/modules/core/core.module.ts
@@ -1,6 +1,5 @@
 import { HeaderV2Component } from './../../components/header-v2/header-v2.component';
 import { FooterComponent } from './../../components/footer/footer.component';
-import { HeaderComponent } from './../../components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,12 +10,10 @@ import { CommonModule } from '@angular/common';
     RouterModule
   ],
   declarations: [
-    HeaderComponent,
     FooterComponent,
     HeaderV2Component
   ],
   exports: [
-    HeaderComponent,
     FooterComponent,
     HeaderV2Component
   ]
