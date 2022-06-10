0d768fdef96059a121594ecc9576f8638a4d2760
harveybland <harvey.bland@genius.online>
2022-06-10 13:41:31 +0100

    added google map

--- src/app/ui/contact/contact.component.ts
+++ src/app/ui/contact/contact.component.ts
@@ -12,10 +12,42 @@ export class ContactComponent implements OnInit {
   faInstagram = faInstagram;
   faFacebook = faFacebook;
 
+  zoom = 15;
+  center!: google.maps.LatLngLiteral;
+  markers: any;
+  options: google.maps.MapOptions = {
+    scrollwheel: true,
+    mapTypeControl: false,
+    streetViewControl: false,
+  }
+
+
+  markerOptions!: google.maps.MarkerOptions;
+  markerPosition!: google.maps.LatLngLiteral;
+
   constructor() { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.center = {
+      lat: 53.86527143576552,
+      lng: -2.3945739053711863
+    }
+
+    this.markerOptions = {
+      draggable: false,
+      label: {
+        color: 'black',
+        text: 'Aurora Beauty'
+      }
+    };
+
+    this.markerPosition = {
+      lat: 53.86527143576552,
+      lng: -2.3945739053711863
+    }
+
   }
+
 
 }
