607d83adda4eb18a45a0ead2394066942e2f3d73
harveybland <harvey.bland@genius.online>
2022-06-14 13:28:27 +0100

    home animation

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -23,6 +23,42 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.animation()
+  }
+
+  animation() {
+    const targets = document.querySelectorAll('.animation');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+
+    function callback(entries: any, observer: any) {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          console.log(entry.target);
+          addActiveClass(entry.target);
+        } else if (!entry.isIntersecting) {
+          removeActiveClass(entry.target);
+        }
+      });
+    }
+
+    function addActiveClass(element: any) {
+      element.classList.add('on');
+    }
+
+    function removeActiveClass(element: any) {
+      element.classList.remove('on');
+    }
   }
 
   openDialog(): void {
